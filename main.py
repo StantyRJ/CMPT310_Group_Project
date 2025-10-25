0d36d3a4ba9f58f0e77f1c197768063a542bfa0b
Akira Egawa <lucian.ming.chen@gmail.com>
2025-10-24 18:05:35 -0700

    changed KNN testing to function (easier to use)

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from lib.KNN import KNN
 
 image_dir = os.path.join("data", "distorted")
 
-data = []
+def testKNN(image_dir):
+    data = []
 
-# Read all files in the folder
-for filename in os.listdir(image_dir):
-    if filename.lower().endswith(".png"): # Check if it's png
-        label = filename[0] # Label with first letter
-        filepath = os.path.join(image_dir,filename) # get the real path
-        try:
-            img = Image.open(filepath).convert("L") # Open it as img
-            data.append((img,label))
-        except Exception as e:
-            print(f"Skipping {filename}: {e}")
+    # Read all files in the folder
+    for filename in os.listdir(image_dir):
+        if filename.lower().endswith(".png"): # Check if it's png
+            label = filename[0] # Label with first letter
+            filepath = os.path.join(image_dir,filename) # get the real path
+            try:
+                img = Image.open(filepath).convert("L") # Open it as img
+                data.append((img,label))
+            except Exception as e:
+                print(f"Skipping {filename}: {e}")
 
-print(f"Loaded {len(data)} images") # Hopefully this > 0
+    print(f"Loaded {len(data)} images") # Hopefully this > 0
 
-# Run KNN
-if len(data) > 1:
-    print(KNN(data, 5))
+    # Run KNN
+    if len(data) > 1:
+        print(KNN(data, 5))
+
+
+testKNN(image_dir)
